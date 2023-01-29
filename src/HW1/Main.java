0f22982a18864cb05968fe4c8327fbd20d9cd477
Sina <sadeghesfahani.sina@gmail.com>
2023-01-29 18:15:00 +0330

    feat(first): first homework implemented

--- src/HW1/Main.java
+++ src/HW1/Main.java
@@ -21,6 +21,8 @@ class Factory {
         switch (command){
             case "first":
                 return new First();
+            case "second":
+                return new Second();
 
         }
         return null;
@@ -33,13 +35,36 @@ interface Command {
 
 
 class First implements Command {
+    protected int rate;
+
+    @Override
+    public void execute() {
+        System.out.print("Please enter your salary income: ");
+        int income = Console.get.nextInt();
+        if(income<0){
+            System.out.println("wrong salary");
+        }else{
+            if(income < 3_000_000){
+                rate = 5;
+            }else if(income < 6_000_000){
+                rate = 10;
+            }else {
+                rate = 24;
+            }
+            System.out.println("Net income will be: "+ ((double) income * (1 - (double) rate/100)) + "$");
+        }
+
+    }
+}
+
+
+class Second implements Command{
 
     @Override
     public void execute() {
 
     }
 }
-
 class Console {
     public static Scanner get = new Scanner(System.in);
 }
