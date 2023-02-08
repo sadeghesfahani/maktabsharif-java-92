6f3076df66083df070741ab95c8809ae2586a3b7
Sina <sadeghesfahani.sina@gmail.com>
2023-02-08 09:28:28 +0330

    feat(HW2): Q3 implemented

---
+++ src/HW2/third.java
@@ -0,0 +1,37 @@
+package HW2;
+
+public class third {
+public static int calculateWaterLevel(int[] heights) {
+    int waterLevel = 0;
+    int n = heights.length;
+    int leftMax = 0;
+    int rightMax = 0;
+    int left = 0;
+    int right = n - 1;
+    while (left < right) {
+        if (heights[left] < heights[right]) {
+            if (heights[left] > leftMax) {
+                leftMax = heights[left];
+            } else {
+                waterLevel += leftMax - heights[left];
+            }
+            left++;
+        } else {
+            if (heights[right] > rightMax) {
+                rightMax = heights[right];
+            } else {
+                waterLevel += rightMax - heights[right];
+            }
+            right--;
+        }
+    }
+    return waterLevel;
+}
+
+    public static void main(String[] args) {
+        int[] heights = {1, 2, 0, 3, 1, 0, 1, 2, 2, 1, 2, 0, 1};
+        int waterLevel = calculateWaterLevel(heights);
+        System.out.println("Water level: " + waterLevel);
+    }
+
+}
